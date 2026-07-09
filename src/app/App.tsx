d185dc40a8a6484a35ac91fdbcecf64203eb8b9a
Jean B <comscijb@gmail.com>
2026-07-09 17:41:45 -0300

    Added ScrollToTop.tsx with internal pages going back to top upon entering

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,14 +3,18 @@ import { HomePage } from "@/pages/HomePage"
 import { LinksPage } from "@/pages/LinksPage"
 import { ProjectPage } from "@/pages/ProjectPage"
 import { NotFoundPage } from "@/pages/NotFoundPage"
+import { ScrollToTop } from "@/components/common/ScrollToTop"
 
 export default function App() {
   return (
-    <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/links" element={<LinksPage />} />
-      <Route path="/projetos/:slug" element={<ProjectPage />} />
-      <Route path="*" element={<NotFoundPage />} />
-    </Routes>
+    <>
+      <ScrollToTop />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/links" element={<LinksPage />} />
+          <Route path="/projetos/:slug" element={<ProjectPage />} />
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>
+    </>
   )
 }
